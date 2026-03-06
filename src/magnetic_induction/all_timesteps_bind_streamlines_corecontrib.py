d3af098d34d837fadcfad06bc4e42deb132afbe5
Dany Waller <40499968+lunarswirls@users.noreply.github.com>
2026-03-06 08:07:41 +0100

    slightly faster solver

--- src/magnetic_induction/all_timesteps_bind_streamlines_corecontrib.py
+++ src/magnetic_induction/all_timesteps_bind_streamlines_corecontrib.py
@@ -19,7 +19,6 @@ plotlog = True
 branch = "HNHV"
 case = "PN"
 
-# timesteps (rows of figure)
 steps = [115000, 142000, 174000, 350000]
 
 case_r = f"R{case}"
@@ -53,109 +52,137 @@ os.makedirs(output_folder, exist_ok=True)
 
 def load_field(ncfile, var_ext):
     ds = xr.open_dataset(ncfile)
+
     x = ds["Nx"].values
     y = ds["Ny"].values
     z = ds["Nz"].values
 
-    Jx = np.transpose(ds[var_ext[0]].isel(time=0).values, (2, 1, 0))
-    Jy = np.transpose(ds[var_ext[1]].isel(time=0).values, (2, 1, 0))
-    Jz = np.transpose(ds[var_ext[2]].isel(time=0).values, (2, 1, 0))
+    Jx = np.transpose(ds[var_ext[0]].isel(time=0).values, (2,1,0))
+    Jy = np.transpose(ds[var_ext[1]].isel(time=0).values, (2,1,0))
+    Jz = np.transpose(ds[var_ext[2]].isel(time=0).values, (2,1,0))
 
     ds.close()
-    return x, y, z, Jx, Jy, Jz
+
+    return x,y,z,Jx,Jy,Jz
+
 
 def idx_range(coord_km, lo, hi):
     i0 = int(np.searchsorted(coord_km, lo, side="left"))
     i1 = int(np.searchsorted(coord_km, hi, side="right"))
-    i0 = max(i0, 0)
-    i1 = min(i1, coord_km.size)
-    return slice(i0, i1)
+    i0 = max(i0,0)
+    i1 = min(i1,coord_km.size)
+    return slice(i0,i1)
+
 
 # --------------------------
-# POISSON SOLVER
+# POISSON SOLVER CACHE
 # --------------------------
 
-def B_from_J_poisson_lowmem(x_km, y_km, z_km, Jx_nA, Jy_nA, Jz_nA):
+poisson_cache = {}
 
-    x = x_km * 1e3
-    y = y_km * 1e3
-    z = z_km * 1e3
 
-    Jx = Jx_nA * 1e-9
-    Jy = Jy_nA * 1e-9
-    Jz = Jz_nA * 1e-9
+def build_poisson_solver(Nx,Ny,Nz,dx,dy,dz):
 
-    dx = np.mean(np.diff(x))
-    dy = np.mean(np.diff(y))
-    dz = np.mean(np.diff(z))
-
-    Nx, Ny, Nz = Jx.shape
-    N = Nx * Ny * Nz
-
-    def D2(n, h):
-        main = -2 * np.ones(n)
+    def D2(n,h):
+        main = -2*np.ones(n)
         off = np.ones(n-1)
-        return sp.diags([off, main, off], [-1, 0, 1], shape=(n, n)) / (h*h)
+        return sp.diags([off,main,off],[-1,0,1],shape=(n,n))/(h*h)
 
-    Lx = D2(Nx, dx)
-    Ly = D2(Ny, dy)
-    Lz = D2(Nz, dz)
+    Lx = D2(Nx,dx)
+    Ly = D2(Ny,dy)
+    Lz = D2(Nz,dz)
 
     Ix = sp.eye(Nx)
     Iy = sp.eye(Ny)
     Iz = sp.eye(Nz)
 
-    Lap = sp.kron(sp.kron(Lx, Iy), Iz) \
-          + sp.kron(sp.kron(Ix, Ly), Iz) \
-          + sp.kron(sp.kron(Ix, Iy), Lz)
+    Lap = sp.kron(sp.kron(Lx,Iy),Iz) \
+        + sp.kron(sp.kron(Ix,Ly),Iz) \
+        + sp.kron(sp.kron(Ix,Iy),Lz)
 
     Aop = -Lap
 
     d = Aop.diagonal()
-    d[d == 0] = 1
+    d[d==0] = 1
 
     def Minv(v):
-        return v / d
+        return v/d
 
-    M = spla.LinearOperator((N, N), matvec=Minv)
+    M = spla.LinearOperator((Nx*Ny*Nz,Nx*Ny*Nz),matvec=Minv)
+
+    return Aop,M
+
+
+# --------------------------
+# POISSON SOLVER
+# --------------------------
+
+def B_from_J_poisson_lowmem(x_km,y_km,z_km,Jx_nA,Jy_nA,Jz_nA):
+
+    x = x_km*1e3
+    y = y_km*1e3
+    z = z_km*1e3
+
+    Jx = Jx_nA*1e-9
+    Jy = Jy_nA*1e-9
+    Jz = Jz_nA*1e-9
+
+    dx = np.mean(np.diff(x))
+    dy = np.mean(np.diff(y))
+    dz = np.mean(np.diff(z))
+
+    Nx,Ny,Nz = Jx.shape
+    N = Nx*Ny*Nz
+
+    key = (Nx,Ny,Nz,dx,dy,dz)
+
+    if key not in poisson_cache:
+
+        print("Building Poisson matrix...")
+
+        poisson_cache[key] = build_poisson_solver(Nx,Ny,Nz,dx,dy,dz)
+
+    Aop,M = poisson_cache[key]
 
     def solve(J):
-        b = (MU0 * J).ravel()
-        sol, _ = spla.cg(Aop, b, maxiter=10000, M=M)
-        return sol.reshape((Nx, Ny, Nz))
+        b = (MU0*J).ravel()
+        sol,_ = spla.cg(Aop,b,maxiter=10000,M=M)
+        return sol.reshape((Nx,Ny,Nz))
 
     Ax = solve(Jx)
     Ay = solve(Jy)
     Az = solve(Jz)
 
-    dAz_dy = np.gradient(Az, dy, axis=1)
-    dAy_dz = np.gradient(Ay, dz, axis=2)
+    dAz_dy = np.gradient(Az,dy,axis=1)
+    dAy_dz = np.gradient(Ay,dz,axis=2)
 
-    dAx_dz = np.gradient(Ax, dz, axis=2)
-    dAz_dx = np.gradient(Az, dx, axis=0)
+    dAx_dz = np.gradient(Ax,dz,axis=2)
+    dAz_dx = np.gradient(Az,dx,axis=0)
 
-    dAy_dx = np.gradient(Ay, dx, axis=0)
-    dAx_dy = np.gradient(Ax, dy, axis=1)
+    dAy_dx = np.gradient(Ay,dx,axis=0)
+    dAx_dy = np.gradient(Ax,dy,axis=1)
 
     Bx = dAz_dy - dAy_dz
     By = dAx_dz - dAz_dx
     Bz = dAy_dx - dAx_dy
 
-    return Bx, By, Bz
+    return Bx,By,Bz
+
 
 # --------------------------
 # STREAMLINE PANEL
 # --------------------------
 
-def add_panel(ax, x1d, y1d, U, V, R, title):
-    X, Y = np.meshgrid(x1d, y1d, indexing="xy")
-    C = np.sqrt(U*U + V*V)
+def add_panel(ax,x1d,y1d,U,V,R,title):
+
+    C = np.sqrt(U*U+V*V)
+
     if plotlog:
-        C = np.log10(C + 1e-12)
+        C = np.log10(C+1e-12)
 
     sp_stream = ax.streamplot(
-        x1d / R,
-        y1d / R,
+        x1d/R,
+        y1d/R,
         U,
         V,
         color=C,
@@ -163,31 +190,41 @@ def add_panel(ax, x1d, y1d, U, V, R, title):
         cmap="viridis"
     )
 
-    th = np.linspace(0, 2*np.pi, 400)
-    ax.plot(np.cos(th), np.sin(th), color="mediumorchid")
-    Rc = 2080 / R
-    ax.plot(Rc*np.cos(th), Rc*np.sin(th), color="hotpink")
+    th = np.linspace(0,2*np.pi,400)
+
+    ax.plot(np.cos(th),np.sin(th),color="mediumorchid")
+
+    Rc = 2080/R
+    ax.plot(Rc*np.cos(th),Rc*np.sin(th),color="hotpink")
 
     ax.set_title(title)
     ax.set_aspect("equal")
 
     return sp_stream
 
+
 # --------------------------
 # FIGURES
 # --------------------------
 
 nrows = len(steps)
-fig_stream, ax_stream = plt.subplots(nrows, 3, figsize=(15, 4*nrows), constrained_layout=True)
-fig_parallel, ax_parallel = plt.subplots(nrows, 3, figsize=(15, 4*nrows), constrained_layout=True)
+
+fig_stream,ax_stream = plt.subplots(
+    nrows,3,figsize=(15,4*nrows),constrained_layout=True
+)
+
+fig_parallel,ax_parallel = plt.subplots(
+    nrows,3,figsize=(15,4*nrows),constrained_layout=True
+)
+
 
 # --------------------------
 # MAIN LOOP
 # --------------------------
 
-for row, step in enumerate(steps):
+for row,step in enumerate(steps):
 
-    print("Processing step", step)
+    print("Processing step",step)
 
     ncfile_r = os.path.join(
         input_folder_r,
@@ -199,15 +236,14 @@ for row, step in enumerate(steps):
         f"Amitis_{case_c}_{filetag}_{step}_merged_4RM.nc"
     )
 
-    x_km, y_km, z_km, Jx_r, Jy_r, Jz_r = load_field(ncfile_r, ["Jx","Jy","Jz"])
-    _, _, _, BxTot_r, ByTot_r, BzTot_r = load_field(ncfile_r, ["Bx_tot","By_tot","Bz_tot"])
-    _, _, _, BxExt_r, ByExt_r, BzExt_r = load_field(ncfile_r, ["Bx","By","Bz"])
+    x_km,y_km,z_km,Jx_r,Jy_r,Jz_r = load_field(ncfile_r,["Jx","Jy","Jz"])
+    _,_,_,BxTot_r,ByTot_r,BzTot_r = load_field(ncfile_r,["Bx_tot","By_tot","Bz_tot"])
+    _,_,_,BxExt_r,ByExt_r,BzExt_r = load_field(ncfile_r,["Bx","By","Bz"])
 
-    _, _, _, Jx_c, Jy_c, Jz_c = load_field(ncfile_c, ["Jx","Jy","Jz"])
-    _, _, _, BxTot_c, ByTot_c, BzTot_c = load_field(ncfile_c, ["Bx_tot","By_tot","Bz_tot"])
-    _, _, _, BxExt_c, ByExt_c, BzExt_c = load_field(ncfile_c, ["Bx","By","Bz"])
+    _,_,_,Jx_c,Jy_c,Jz_c = load_field(ncfile_c,["Jx","Jy","Jz"])
+    _,_,_,BxTot_c,ByTot_c,BzTot_c = load_field(ncfile_c,["Bx_tot","By_tot","Bz_tot"])
+    _,_,_,BxExt_c,ByExt_c,BzExt_c = load_field(ncfile_c,["Bx","By","Bz"])
 
-    # differences
     Bx_core = BxTot_r - BxExt_r
     By_core = ByTot_r - ByExt_r
     Bz_core = BzTot_r - BzExt_r
@@ -220,45 +256,46 @@ for row, step in enumerate(steps):
     By_diff = ByTot_c - ByTot_r
     Bz_diff = BzTot_c - BzTot_r
 
-    sx = idx_range(x_km, -L-buf, L+buf)
-    sy = idx_range(y_km, -L-buf, L+buf)
-    sz = idx_range(z_km, -L-buf, L+buf)
+    sx = idx_range(x_km,-L-buf,L+buf)
+    sy = idx_range(y_km,-L-buf,L+buf)
+    sz = idx_range(z_km,-L-buf,L+buf)
 
     x_sub = x_km[sx]
     y_sub = y_km[sy]
     z_sub = z_km[sz]
 
-    Bx_sub, By_sub, Bz_sub = B_from_J_poisson_lowmem(
-        x_sub, y_sub, z_sub,
-        Jx[sx, sy, sz],
-        Jy[sx, sy, sz],
-        Jz[sx, sy, sz]
+    Bx_sub,By_sub,Bz_sub = B_from_J_poisson_lowmem(
+        x_sub,y_sub,z_sub,
+        Jx[sx,sy,sz],
+        Jy[sx,sy,sz],
+        Jz[sx,sy,sz]
     )
 
-    sx2 = idx_range(x_sub, -L, L)
-    sy2 = idx_range(y_sub, -L, L)
-    sz2 = idx_range(z_sub, -L, L)
+    sx2 = idx_range(x_sub,-L,L)
+    sy2 = idx_range(y_sub,-L,L)
+    sz2 = idx_range(z_sub,-L,L)
 
     x_box = x_sub[sx2]
     y_box = y_sub[sy2]
     z_box = z_sub[sz2]
 
-    Bx_box = Bx_sub[sx2, sy2, sz2]
-    By_box = By_sub[sx2, sy2, sz2]
-    Bz_box = Bz_sub[sx2, sy2, sz2]
+    Bx_box = Bx_sub[sx2,sy2,sz2]
+    By_box = By_sub[sx2,sy2,sz2]
+    Bz_box = Bz_sub[sx2,sy2,sz2]
 
-    Bxcore = Bx_core[sx2, sy2, sz2]
-    Bycore = By_core[sx2, sy2, sz2]
-    Bzcore = Bz_core[sx2, sy2, sz2]
+    Bxcore = Bx_core[sx2,sy2,sz2]
+    Bycore = By_core[sx2,sy2,sz2]
+    Bzcore = Bz_core[sx2,sy2,sz2]
 
-    Bxdiff = Bx_diff[sx2, sy2, sz2]
-    Bydiff = By_diff[sx2, sy2, sz2]
-    Bzdiff = Bz_diff[sx2, sy2, sz2]
+    Bxdiff = Bx_diff[sx2,sy2,sz2]
+    Bydiff = By_diff[sx2,sy2,sz2]
+    Bzdiff = Bz_diff[sx2,sy2,sz2]
 
-    Bcore_mag = np.sqrt(Bxcore**2 + Bycore**2 + Bzcore**2) + 1e-30
-    bxhat = Bxcore / Bcore_mag
-    byhat = Bycore / Bcore_mag
-    bzhat = Bzcore / Bcore_mag
+    Bcore_mag = np.sqrt(Bxcore**2+Bycore**2+Bzcore**2)+1e-30
+
+    bxhat = Bxcore/Bcore_mag
+    byhat = Bycore/Bcore_mag
+    bzhat = Bzcore/Bcore_mag
 
     Bind_parallel = Bxdiff*bxhat + Bydiff*byhat + Bzdiff*bzhat
 
@@ -266,61 +303,69 @@ for row, step in enumerate(steps):
     iy0 = np.argmin(np.abs(y_box))
     iz0 = np.argmin(np.abs(z_box))
 
-    # streamlines
-    Bx_xy = Bx_box[:, :, iz0].T * 1e9
-    By_xy = By_box[:, :, iz0].T * 1e9
+    Bx_xy = Bx_box[:,:,iz0].T*1e9
+    By_xy = By_box[:,:,iz0].T*1e9
 
-    Bx_xz = Bx_box[:, iy0, :].T * 1e9
-    Bz_xz = Bz_box[:, iy0, :].T * 1e9
+    Bx_xz = Bx_box[:,iy0,:].T*1e9
+    Bz_xz = Bz_box[:,iy0,:].T*1e9
 
-    By_yz = By_box[ix0, :, :].T * 1e9
-    Bz_yz = Bz_box[ix0, :, :].T * 1e9
+    By_yz = By_box[ix0,:,:].T*1e9
+    Bz_yz = Bz_box[ix0,:,:].T*1e9
 
-    sp_stream = add_panel(ax_stream[row, 0], x_box, y_box, Bx_xy, By_xy, RM, "XY")
-    add_panel(ax_stream[row, 1], x_box, z_box, Bx_xz, Bz_xz, RM, "XZ")
-    add_panel(ax_stream[row, 2], y_box, z_box, By_yz, Bz_yz, RM, "YZ")
+    sp_stream = add_panel(ax_stream[row,0],x_box,y_box,Bx_xy,By_xy,RM,"XY")
+    add_panel(ax_stream[row,1],x_box,z_box,Bx_xz,Bz_xz,RM,"XZ")
+    add_panel(ax_stream[row,2],y_box,z_box,By_yz,Bz_yz,RM,"YZ")
 
-    # scalar panels
-    Bp_xy = Bind_parallel[:, :, iz0].T
-    Bp_xz = Bind_parallel[:, iy0, :].T
-    Bp_yz = Bind_parallel[ix0, :, :].T
+    Bp_xy = Bind_parallel[:,:,iz0].T
+    Bp_xz = Bind_parallel[:,iy0,:].T
+    Bp_yz = Bind_parallel[ix0,:,:].T
 
-    v = np.nanpercentile(np.abs(Bind_parallel), 99)
+    v = np.nanpercentile(np.abs(Bind_parallel),99)
 
-    im = ax_parallel[row, 0].imshow(
+    im = ax_parallel[row,0].imshow(
         Bp_xy,
         origin="lower",
-        extent=[x_box[0]/RM, x_box[-1]/RM, y_box[0]/RM, y_box[-1]/RM],
+        extent=[x_box[0]/RM,x_box[-1]/RM,y_box[0]/RM,y_box[-1]/RM],
         cmap="RdBu_r",
-        vmin=-v, vmax=v
+        vmin=-v,vmax=v
     )
 
-    ax_parallel[row, 1].imshow(
+    ax_parallel[row,1].imshow(
         Bp_xz,
         origin="lower",
-        extent=[x_box[0]/RM, x_box[-1]/RM, z_box[0]/RM, z_box[-1]/RM],
+        extent=[x_box[0]/RM,x_box[-1]/RM,z_box[0]/RM,z_box[-1]/RM],
         cmap="RdBu_r",
-        vmin=-v, vmax=v
+        vmin=-v,vmax=v
     )
 
-    ax_parallel[row, 2].imshow(
+    ax_parallel[row,2].imshow(
         Bp_yz,
         origin="lower",
-        extent=[y_box[0]/RM, y_box[-1]/RM, z_box[0]/RM, z_box[-1]/RM],
+        extent=[y_box[0]/RM,y_box[-1]/RM,z_box[0]/RM,z_box[-1]/RM],
         cmap="RdBu_r",
-        vmin=-v, vmax=v
+        vmin=-v,vmax=v
     )
 
-    # rotated timestep label
-    ax_stream[row, 0].text(-0.28, 0.5, f"t={step*0.002:.1f}s",
-                            transform=ax_stream[row, 0].transAxes,
-                            rotation=90, va="center", ha="center", fontsize=12)
+    ax_stream[row,0].text(
+        -0.28,0.5,f"t={step*0.002:.1f}s",
+        transform=ax_stream[row,0].transAxes,
+        rotation=90,
+        va="center",
+        ha="center",
+        fontsize=12
+    )
 
-    ax_parallel[row, 0].text(-0.28, 0.5, f"t={step*0.002:.1f}s",
-                              transform=ax_parallel[row, 0].transAxes,
-                              rotation=90, va="center", ha="center", fontsize=12)
+    ax_parallel[row,0].text(
+        -0.28,0.5,f"t={step*0.002:.1f}s",
+        transform=ax_parallel[row,0].transAxes,
+        rotation=90,
+        va="center",
+        ha="center",
+        fontsize=12
+    )
 
     gc.collect()
+
 
 # --------------------------
 # COLORBARS
@@ -331,14 +376,15 @@ fig_stream.colorbar(
     ax=ax_stream.ravel().tolist(),
     fraction=0.02,
     pad=0.02
-).set_label("|B_ind|")
+).set_label("|B$_{ind}$|")
 
 fig_parallel.colorbar(
     im,
     ax=ax_parallel.ravel().tolist(),
     fraction=0.02,
     pad=0.02
-).set_label("B$_{ind}$ · B_core")
+).set_label("B$_{ind}$ · B$_{d}$")
+
 
 # --------------------------
 # SAVE
@@ -347,7 +393,14 @@ fig_parallel.colorbar(
 fig_stream.suptitle(f"{case_c}-{case_r} {branch} B$_{{ind}}$ streamlines")
 fig_parallel.suptitle(f"{case_c}-{case_r} {branch} B$_{{ind}}$ influence on core")
 
-fig_stream.savefig(os.path.join(output_folder, "Bind_streamlines_timesteps.png"), dpi=300)
-fig_parallel.savefig(os.path.join(output_folder, "Bind_parallel_timesteps.png"), dpi=300)
+fig_stream.savefig(
+    os.path.join(output_folder,"Bind_streamlines_timesteps.png"),
+    dpi=300
+)
+
+fig_parallel.savefig(
+    os.path.join(output_folder,"Bind_parallel_timesteps.png"),
+    dpi=300
+)
 
 print("Finished")
